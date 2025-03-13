60227318dbdc120c16f3ec022819a45c605ab255
Senura Weerasinghe <senuranisal1998@gmail.com>
2025-03-13 11:38:37 +0000

    added updatepic and check apis

--- backend/src/controllers/auth.contoller.js
+++ backend/src/controllers/auth.contoller.js
@@ -1,6 +1,7 @@
 import { generateToken } from '../lib/utils.js'
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
+import cloudinary from '../lib/cloudinary.js'
 
 export const signup=async (req,res)=>{
     const {fullName,email,password}=req.body
@@ -67,9 +68,9 @@ export const login= async (req,res)=>{
         generateToken(user._id,res)
         res.status(200).json({
             _id:user._id,
-            fullName,
+            fullName:user.fullName,
             email,
-            profilePic
+            profilePic:user.profilePic
 
         })
     }catch (error){
@@ -86,5 +87,34 @@ export const logout=(req,res)=>{
         res.status(200).json({message:"Logged out successfully"})
     }catch (error){
         console.log("Error in logout controller",error.message)
+        res.status(500).json({message:'internal server error'})
+    }
+}
+
+export const updateProfile=async (req,res)=>{
+    try{
+        const {profilePic}=req.body
+        const userId=req.user._id
+        if(!profilePic){
+            return res.status(400).json({message:"Profile pic is required"})
+        }
+
+        const uploadResponse=await cloudinary.uploader.upload(profilePic)
+        const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:True})
+        res.status(200).json(updatedUser)
+    }catch (error){
+        console.log('error in update profile controller',error.message)
+        res.status(500).json({message:'internal server error'})
+    }
+}
+
+
+export const check=(req,res)=>{
+    try{
+        res.status(200).json(req.user)
+    }catch (error){
+        console.log("Error in checkAuth controller",error.message)
+        res.status(500).json({message:"Internal Server Error"})
+
     }
 }
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,8 @@
 import express from "express"
 import { login,logout,signup } from "../controllers/auth.contoller.js"
+import { protectRoute } from "../middleware/auth.middleware.js"
+import { updateProfile } from "../controllers/auth.contoller.js"
+import { check } from "../controllers/auth.contoller.js"
 
 const router = express.Router()
 
@@ -8,5 +11,8 @@ router.post("/signup",signup)
 router.post("/login",login)
 
 router.post("/logout",logout)
+
+router.put("/update-profile",protectRoute,updateProfile)
+router.get("/check",protectRoute,check)
 
 export default router
