97c6508faed11a836f4b12fae393d6a55919e2e6
Senura Weerasinghe <senuranisal1998@gmail.com>
2025-03-15 12:39:56 +0000

    created message apis and in the frontend added signup page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import dotenv from 'dotenv'
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.routes.js"
 import { connectDB } from "./lib/db.js"
 import cookieParser from 'cookie-parser'
+import cors from "cors"
 
 dotenv.config()
 
@@ -13,8 +15,14 @@ const PORT=process.env.PORT
 app.use(express.json())
 
 app.use(cookieParser())
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+
+}))
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT,()=>{
     console.log('server is running on port 5001')
